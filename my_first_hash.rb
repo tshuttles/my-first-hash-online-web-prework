e8ede7c3071003d6d5b4f8e6dcde3b88222a4c6f
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-15 05:00:40 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,17 +20,17 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  shipping_manifest
+  shipping_manifest["oil paintings"]
 end
 
 def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
-                  "oil paintings" => 3
+                  "oil paintings" => 3,
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+  
 
 
   # add 4 gun powder to the shipping_manifest hash below
