2b5168ef11b12c40d566a80de7e5ac6341849e85
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-15 05:01:26 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,7 +30,8 @@ def adding
                   "oil paintings" => 3,
                  }
   # add 2 muskets to the shipping_manifest hash below
-  
+  shipping_manifest["muskets"] = 2 
+  shipping_manifest["gun powder"] = 4
 
 
   # add 4 gun powder to the shipping_manifest hash below
@@ -38,5 +39,5 @@ def adding
 
 
   # return the shipping_manifest hash below
-
+  return shipping_manifest
 end
